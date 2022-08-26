a61655dc454c70e765ac08855ad0444f510dd46b
ImonEmmanuel <emmanuelpraise42@gmail.com>
2022-08-26 10:53:56 +0100

    Auth

--- Model/CityWithoutPointOfInterestDto.cs
+++ Model/CityWithoutPointOfInterestDto.cs
@@ -1,10 +1,22 @@
 ﻿namespace CItyInfo.API.Model
 {
+    /// <summary>
+    /// A DTO for a city without Point of Interest
+    /// </summary>
     public class CityWithoutPointOfInterestDto
     {
+        /// <summary>
+        /// The Id of the City
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// The Name of the City
+        /// </summary>
         public string Name { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The Description of the City
+        /// </summary>
         public string? Description { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,10 +2,15 @@ using CItyInfo.API.Contract;
 using CItyInfo.API.DbContexts;
 using CItyInfo.API.Model;
 using CItyInfo.API.Service;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Serilog;
+using System.Reflection;
+using System.Text;
 
 Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
     .WriteTo.Console()
@@ -35,12 +40,70 @@ builder.Services.AddDbContext<CityInfoContext>(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen( options =>
+{
+    var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
+
+    options.IncludeXmlComments(xmlCommentPath);
+
+    options.AddSecurityDefinition("CityInfoApiBearerAuth", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        Description = "Input a Valid token to access this API"
+
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "CityInfoApiBearerAuth"
+                }
+            },new List<string>()
+        }
+
+    });
+
+    
+});
+
 builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 builder.Services.AddSingleton<CitiesDataStore>();
 builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(option =>
+{
+    //bearer.Audience = builder.Configuration["Authetication:Audience"];
+    //bearer.Authority = builder.Configuration["Authetication:Issuer"];
+    option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Authetication:Issuer"],
+        ValidAudience = builder.Configuration["Authetication:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Authetication:SecretForKey"]))
+    };
+    
+});
+
+builder.Services.AddApiVersioning(setup =>
+{
+    setup.AssumeDefaultVersionWhenUnspecified = true;
+    setup.DefaultApiVersion = new ApiVersion(1, 0);
+    setup.ReportApiVersions = true;
+});
+
 
 #if DEBUG
 builder.Services.AddTransient<ILocalMailService , LocalMailService>();
@@ -57,10 +120,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
 
 app.UseRouting();
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 
